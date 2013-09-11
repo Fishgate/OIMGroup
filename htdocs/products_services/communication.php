096a18af89cf3e0a55cc15073a0d9ed44c0f6fc0
Jan <jpretorius79@gmail.com>
2013-09-11 15:50:21 +0200

    New Product Pages

---
+++ htdocs/products_services/communication.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+    <head>
+        <!-- BEGIN HEAD -->
+        <?php include '../includes/styles_generic.php' ?>
+        <!-- END HEAD -->
+    </head>
+    <body>
+        <!--[if lt IE 7]>
+            <p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
+        <![endif]-->
+
+        <!-- BEGIN HEADER -->
+        <header id='header'>
+            <?php include '../includes/header_products.php' ?>
+        </header>
+        <!-- END HEADER -->
+
+        <!-- BEGIN BODY -->
+        <div id='contentwrap' class='clearfix'>
+            <div id='body' class='left'>
+                <div class='breadcrumb'><i class="icon-map-marker icon-large red"></i> &nbsp;YOU ARE HERE: <a href='#'>Products & Services</a> / <span class='red'>Strategic and Change Communication</span></div>
+                <h1>Strategic and Change Communication</h1>
+                <div class='caption'>Create clarity in your business</div>
+                <div class='copy'>
+                    <p>Communication is the golden thread that runs through the implementation of any organisational strategy or change initiative. It ties everything together from start to finish and ensure clarity among all.
+
+Communication’s ultimate success, however, is closely linked to two aspects:
+⦁	Alignment with your business strategy.
+⦁	Effective employee engagement to share information, create understanding and effect mindset change to acquire the subsequent behaviour and culture change.
+
+Our approach to strategic and change communication addresses these two crucial aspects.
+
+Our offering
+We provide a unique approach to communication that engages employees, namely:
+⦁	Information sharing and awareness: Using a range of communication tools, eg social networking, road shows, newsletters, posters and communication campaigns.
+⦁	Peer-to-peer influence: Establish change champions and peer-level leaders who share information, create buy-in and influence on an informal level
+⦁	Business understanding: Coupled with information sharing and peer champions, we use OIM’s Climate Creation Workshops to ensure all employees have a holistic understanding of the business context and how their role contributes to organisational success. Similarly, OIM’s employee engagement methodology, INVOCOMS, is used to engage teams around business goals, team conduct and specific focused communication and learning topics.
+
+The integration of these elements results in an informed, engaged workforce that is motivated to be part of the unfolding change within the organisation.
+
+Below is an overview of the main offerings that work in partnership to provide this unique approach.
+
+⦁	Strategic communication
+Aim: A clearly defined strategic plan to influence stakeholders positively through communication
+
+Traditional strategic communication uses a hierarchical method of information sharing. This is done in a variety of ways such as presentations, workshops and electronic communication. While information sharing is important, it often lacks engagement, tends to create the impression of “being spoken to”, and commonly does not direct the right message in the right way to the right people. To address these shortcomings, OIM In Context establishes a clearly defined communication framework based on the following key elements:
+⦁	Stakeholder analysis
+⦁	Communication climate check (by means of survey tools)
+⦁	Key message development
+⦁	Key message delivery
+
+⦁	Change communication
+Aim: Support new business initiatives through a change communication plan that accurately informs people of the upcoming change, creates understanding around it and establishes buy-in. 
+Change communication plays a crucial role in any change initiative and is an important key to sustainable behavioural and culture change.
+
+Our offering comprises the following elements:
+⦁	Change readiness assessment
+⦁	Stakeholder map and key message design
+⦁	Informal social network 
+⦁	Peer-to-peer influence
+People are influenced by what they see and behaviours are copied. Therefore we train change champions and peer-level leaders.
+
+All the above elements are supported by consultation and training to ensure the full understanding, involvement and cooperation of employees. 
+
+The OIM In Context difference
+⦁	We are focused on providing practical solutions to deliver results.
+⦁	We partner with our clients to develop plans that are relevant and focused.
+⦁	The offering seamlessly integrates with other OIM products and services.
+⦁	We ensure stakeholder engagement from the start to the end of the process.
+⦁	We pride ourselves in being flexible, always adapting to client’s strategic needs. 
+⦁	Our services are available to small, medium and large organisations.
+⦁	We have access to a local and international network of virtual specialists to draw on relevant expertise when required.
+⦁	We move fast and ensure everyone has fun along the way! 
+
+OIM’s strategic and change communication offerings are offered in partnership with niche consulting company In Context. Link to www.incontext.co.za 
+                    </p>
+                </div>
+            </div>
+            <div id='sidebar' class='left'>SIDEBAR</div>
+        </div>
+        <!-- END BODY -->
+        
+        <!-- BEGIN FOOTER -->
+        <footer>
+            <?php include '../includes/footer.php' ?>
+        </footer>
+        <!-- END FOOTER -->
+    </body>
+</html>
