f627db42283215bbbda38812cd58d09dea32d568
Tyrone <ty@crayoncapsule.co.za>
2013-09-11 10:02:43 +0200

    Tyrone gets ready to pull in Jans first push he worked on stylesheet so conflicts are possibly going to occure there was unique pathing he used eg ../css i will just set it back again locall for me untill i rectify the styles include

--- htdocs/includes/header.php
+++ htdocs/includes/header.php
@@ -30,10 +30,30 @@
     <!-- MEGA MENU METHODOLOGY -->
     <div class="mega-holder clearfix">
         <div class="left mega-thirds">
-            <div class="hega-head"><i class="icon-group red"></i>PEOPLE</div>
+            <div class="mega-head">
+                <i class="icon-group red"></i>PEOPLE
+            </div>
+            <div class="mega-list">
+                <ul>
+                    <li>ONE</li>
+                    <li>ONE
+                        <ul class="mega-flyout">
+                            <li>TWO</li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
         </div>
-        <div class="left mega-thirds"><i class="icon-umbrella red"></i>ORGANISATION</div>
-        <div class="left mega-thirds"><i class="icon-cogs red"></i>OPERATIONS</div>
+        <div class="left mega-thirds">
+            <div class="mega-head">
+                <i class="icon-umbrella red"></i>ORGANISATION
+            </div>
+        </div>
+        <div class="left mega-thirds">
+            <div class="mega-head">
+                <i class="icon-cogs red"></i>OPERATIONS
+            </div>
+        </div>
     </div>
 </nav>
 
