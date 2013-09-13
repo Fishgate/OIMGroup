2ee3338265095afd2fae12df0e20bc8f897fdeb8
Tyrone <ty@crayoncapsule.co.za>
2013-09-13 13:29:06 +0200

    Tyrone build methodology page

--- htdocs/methodology/index.php
+++ htdocs/methodology/index.php
@@ -25,7 +25,13 @@
                 <div class='breadcrumb'><i class="icon-map-marker icon-large red"></i> &nbsp;YOU ARE HERE: <a href='#'>Our Methodology</a></div>
                 <h1>Our Methodology</h1>
                 <div class='copy'>
-                    <p></p>
+                    <p>In the same way Apple understand technology [and Bono knows how to entertain a crowd?], OIM understands how to translate strategy into action and sustainable results in key business areas. This is our field of expertise and why we call ourselves performance specialists.
+</p>
+                    <p>Our approach offers comprehensive business performance solutions, focusing on people management, operational optimisation and organisational improvement in an integrated way.
+</p>
+<p>The ultimate aim is to ensure all stakeholders – customers, suppliers, society, employees and shareholders – benefit over time. This is what OIM calls a Prosperity Partnership&reg;.
+</p>
+                    <p>This approach consists of the following integrated elements for sustainable performance.</p>
                 </div>
             </div>
             <div id='sidebar' class='left'>
@@ -35,9 +41,101 @@
             </div>
             <!-- BEGIN FULL WIDTH SECTION -->
                 <div class="left dotted-top secondary-full-width clearfix">
-                    <div class="left">ONE</div>
-                    <div class="left">22</div>
-                    <div class="left">333</div>
+                    <!--====================== ROW HEAD ============-->
+                    <div class="left meth-block meth-head">
+                        <center>
+                            <h2>Model</h2>
+                        </center>
+                    </div>
+                    <div class="left meth-divider">&nbsp;</div>
+                    <div class="left meth-block meth-head">
+                        <center>
+                            <h2>The Outcomes of this Model are</h2>
+                        </center>
+                    </div>
+                    <div class="left meth-divider">&nbsp;</div>
+                    <div class="left meth-block meth-head">
+                        <center>
+                            <h2>Services and Products</h2>
+                        </center>
+                    </div>
+                    <!--====================== ROW 1 ============-->
+                    <div class="left meth-block bg-blue-dk">Clarity of purpose and direction</div>
+                    <div class="left meth-divider"><i class="icon-angle-right icon-large"></i></div>
+                    <div class="left meth-block bg-blue-dk">Everyone knows where we are going as a company</div>
+                    <div class="left meth-divider"><i class="icon-angle-right icon-large"></i></div>
+                    <div class="left meth-block bg-blue-dk">
+                        <ul>
+                            <li>Business strategy development</li>
+                            <li>Strategic communication strategy</li>
+                            <li>Supplier and customer strategy</li>
+                        </ul>
+                    </div>
+                    <!--====================== ROW 2 ============-->
+                    <div class="left meth-block bg-blue-lt">Structure, alignment and focus</div>
+                    <div class="left meth-divider"><i class="icon-angle-right icon-large"></i></div>
+                    <div class="left meth-block bg-blue-lt">Everyone knows what to focus their energy on daily</div>
+                    <div class="left meth-divider"><i class="icon-angle-right icon-large"></i></div>
+                    <div class="left meth-block bg-blue-lt">
+                        <ul>
+                            <li>Business architecture</li>
+                            <li>Competency profiling</li>
+                            <li>Reward strategy</li>
+                            <li>Change &amp; operational communication strategies</li>
+                        </ul>
+                    </div>
+                    <!--====================== ROW 3 ============-->
+                    <div class="left meth-block bg-red">Effective leadership culture</div>
+                    <div class="left meth-divider"><i class="icon-angle-right icon-large"></i></div>
+                    <div class="left meth-block bg-red">Leaders lead their teams with credibility</div>
+                    <div class="left meth-divider"><i class="icon-angle-right icon-large"></i></div>
+                    <div class="left meth-block bg-red">
+                        <ul>
+                            <li>Leadership competencies and practices definition</li>
+                            <li>Leadership and critical talent assessments</li>
+                            <li>Leadership development</li>
+                            <li>Talent Management</li>
+                        </ul>
+                    </div>
+                    <!--====================== ROW 4 ============-->
+                    <div class="left meth-block bg-green">Stakeholder engagement</div>
+                    <div class="left meth-divider"><i class="icon-angle-right icon-large"></i></div>
+                    <div class="left meth-block bg-green">Everyone is involved, daily, in goal setting, problem solving and planning</div>
+                    <div class="left meth-divider"><i class="icon-angle-right icon-large"></i></div>
+                    <div class="left meth-block bg-green">
+                        <ul>
+                            <li>Employee engagement</li>
+                            <li>Other stakeholder engagement</li>
+                            <li>Employee relations</li>
+                            <li>Diversity management</li>
+                        </ul>
+                    </div>
+                    <!--====================== ROW 5 ============-->
+                    <div class="left meth-block bg-blue-md">Optimisation of business processes, systems, resources and competencies</div>
+                    <div class="left meth-divider"><i class="icon-angle-right icon-large"></i></div>
+                    <div class="left meth-block bg-blue-md">Everyone contributes to quality, cost and service improvements</div>
+                    <div class="left meth-divider"><i class="icon-angle-right icon-large"></i></div>
+                    <div class="left meth-block bg-blue-md">
+                        <ul>
+                            <li>Operational improvement interventions</li>
+                            <li>Continuous improvement processes &amp; practices</li>
+                            <li>Establishment of an innovation culture</li>
+                            <li>Functional competence development</li>
+                            <li>On-the-job operational coaching</li>
+                        </ul>
+                    </div>
+                    <!--====================== ROW 6 ============-->
+                    <div class="left meth-block bg-purple">Measurement feedback, improvement, recognition and reward</div>
+                    <div class="left meth-divider"><i class="icon-angle-right icon-large"></i></div>
+                    <div class="left meth-block bg-purple">Teams regularly review their performance, focus on accountability recognition and reward</div>
+                    <div class="left meth-divider"><i class="icon-angle-right icon-large"></i></div>
+                    <div class="left meth-block bg-purple">
+                        <ul>
+                            <li>Organisational effectiveness measurement</li>
+                            <li>Operational effectiveness measurement</li>
+                            <li>Individual performance management</li>
+                        </ul>
+                    </div>
                 </div>
             <!-- END FULL WIDTH SECTION -->
         </div>
